4ece76cfe6bc57b9cc75e11bb7b520828d9971f4
Onur AKMESE <onur.akmese@viacesi.fr>
2024-09-15 23:53:32 +0200

    Added the basic control with the left and right arrow

--- ncurses.c
+++ ncurses.c
@@ -5,7 +5,7 @@
 
 int main()
 {
-	int x, y;
+	int key, x, y;
 	char letter;
 	initscr();
 	printw("Press any key");
@@ -30,8 +30,36 @@ int main()
 	move(y, x);
 	printw("Over here");
 	refresh();
-
 	getch();
+	clear();
+
+	keypad(stdscr, TRUE);
+	noecho();
+	x = y = 5;
+
+	while(key != 'q')
+	{
+		clear();
+		move(0, 0);
+		printw("Press left or right arrow - exit by pressing: q");
+		
+		move(y, x);
+		printw("0");
+		refresh();
+		
+		key = getch();
+		if(key == KEY_LEFT)
+		{
+			x--;
+			if(x < 0) x=0;
+		}
+		else if(key == KEY_RIGHT)
+		{
+			x++;
+			if(x > 30) x=30;
+		}
+	}
+	
 	endwin();
 
 	return 0;
